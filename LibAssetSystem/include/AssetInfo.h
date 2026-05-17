c03b9207d109de13e3b6dc1545b28e8ab4b65f31
PlutexArch <plutex.xio@gmail.com>
2026-05-17 21:49:13 +0200

    LICENSE.md and README.md

--- LibAssetSystem/include/AssetInfo.h
+++ LibAssetSystem/include/AssetInfo.h
@@ -5,4 +5,22 @@
 #ifndef PLUENGINE_ASSETINFO_H
 #define PLUENGINE_ASSETINFO_H
 
+#include "PluEngine/Core.h"
+#include "AssetInfo.generated.h"
+#include "PluEngine/Objects/EngineObject.h"
+
+namespace Plu
+{
+    PLU_STRUCT()
+    struct PLU_API AssetInfo
+    {
+        REFLECTION_BODY_ASSETINFO()
+
+#ifdef PLU_EDITOR
+        dsad
+#endif
+
+    };
+}
+
 #endif //PLUENGINE_ASSETINFO_H
