d0a1af032e42549eb6ecee7490c6c24d247e0159
PlutexArch <plutex.xio@gmail.com>
2026-09-06 15:49:20 +0200

    idk

--- LibEngine/PluApp/src/Application.cpp
+++ LibEngine/PluApp/src/Application.cpp
@@ -152,11 +152,11 @@ namespace Plu
             float deltaTime = std::chrono::duration<float>(frameStart - lastFrame).count();
             lastFrame = frameStart;
 
-            if (deltaTime > 2.0f) {
+            if (deltaTime > 1.0f) {
 #ifdef PLU_DEBUG
                 PLU_CORE_CRITICAL("Unbelievably high deltaTime, capping to 1");
 #endif
-                deltaTime = 2.0f;
+                deltaTime = 0.99f;
             }
 
             SetMainThreadDeltaTime(deltaTime);
